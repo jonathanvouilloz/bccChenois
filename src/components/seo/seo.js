3119e5f602446dac6b84a4c2dd451e189c472a80
Jonathan Vouilloz <jonathan.vouilloz@gmail.com>
2020-01-02 16:01:22 +0700

    fix favicon

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -22,6 +22,10 @@ const Seo = ({titleD, descriptionD}) => {
         <Helmet htmlAttributes={{lang:"fr"}} title={title + ` | ` +titleD}>
             <meta name="description" content={description + ' - ' +descriptionD}/>
             <meta name="image" content={image} />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"></link>
             <meta name="keywords" content="badminton, chênois, thônex, chêne-bourg, genève, interclub, junior, jeune, bcc, essayer, amusement, sport, enfant, adolescent" />
             <meta name="twitter:creator" content={twitterUsername}/>
         </Helmet>
