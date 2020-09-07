fa844917730dbb1b8b24d448564e3b11a3c6c29d
Jonathan <jonathanvouilloz@gmail.com>
2020-09-07 13:57:06 +0200

    maj numero de tel

--- src/components/joinUs/team/team.js
+++ src/components/joinUs/team/team.js
@@ -14,56 +14,56 @@ const team = () => {
           <MemberBox
             role="Président"
             name="Jean-Philippe ETTER"
-            contact="078.944.77.07"
+            contact="+41 78 699 97 60"
             sentence="-"
             image="team/jp.jpg"
           />
           <MemberBox
             role="Vice-Président"
             name="Julien Ourny"
-            contact="078.944.77.07"
+            contact="+41 76 616 17 74"
             sentence="-"
             image="team/adrien-ourny.png"
           />
           <MemberBox
             role="Responsable Interclub"
             name="Adrien Ourny"
-            contact="078.944.77.07"
+            contact="+41 76 616 17 75"
             sentence=""
             image="team/adrien-ourny.png"
           />
            <MemberBox
             role="Entraineur Junior I"
             name="Guillaume Juillerat"
-            contact="078.944.77.07"
+            contact="+41 79 719 39 81"
             sentence=""
             image="team/adrien-ourny.png"
           />
           <MemberBox
             role="Entraineur Junior II"
             name="Jérémy Apostolides"
-            contact="078.944.77.07"
+            contact="+41 78 607 29 55"
             sentence=""
             image="team/adrien-ourny.png"
           />
           <MemberBox
             role="Entraineur Junior III"
             name="Jonathan Vouilloz"
-            contact="078.944.77.07"
+            contact="+41 78 944 77 07"
             sentence=""
             image="team/jonathan.png"
           />
           <MemberBox
             role="Entraineur Junior IV"
             name="Jonas Garazi"
-            contact="078.944.77.07"
+            contact="+41 76 544 22 32"
             sentence=""
             image="team/adrien-ourny.png"
           />
           <MemberBox
             role="Entraineur Adultes"
             name="Arnaud Ritz"
-            contact="078.944.77.07"
+            contact="+41 78 883 25 83"
             sentence=""
             image="team/arnaud.jpg"
           />     
